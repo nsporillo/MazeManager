802614294be63977200fa7a72a716f9f7d141a50
Nick Porillo <nsporillo@gmail.com>
2015-04-25 19:10:12 -0400

    The menu listener

---
+++ src/MenuListener.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MenuListener implements ActionListener {
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
