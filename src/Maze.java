eba73e3e8c610e0ee862a36e5b7d8302fab4d6d0
Nick Porillo <nsporillo@gmail.com>
2015-05-04 22:11:56 -0400

    Merge editor into same maze window

--- src/Maze.java
+++ src/Maze.java
@@ -1,25 +1,112 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.util.Scanner;
 
-/**
- * Abstract Maze
- * Can be the object for a maze editor or maze solver panel
- */
 public abstract class Maze extends JPanel {
 
-    private File file; // file associated with this maze
+    private File file;             // file associated with this maze
     protected Point start, finish; // start and end points of maze
-    protected int[][] grid; // coordinate system
-    protected int rows, cols; // # of rows and # of colums
+    protected int[][] grid;        // coordinate system
+    protected int rows, cols, rowSize, colSize; // # of rows and # of columns
+    protected TileType tooltip;
+
+    protected boolean paint = false;
 
     public Maze(String mazeName) {
-        file = new File("mazes/" + mazeName); // use maze directory
+        this.file = new File("mazes/" + mazeName); // use maze directory
+        this.tooltip = TileType.START; // default to start position
     }
 
-    public abstract void load(); // load maze from file
+    public void load() {
+        try {
+            Scanner scan = new Scanner(this.file);
+            rows = scan.nextInt();
+            cols = scan.nextInt();
+            colSize = (int) Math.floor((double) getPreferredSize().width / (double) cols);
+            rowSize = (int) Math.floor((double) getPreferredSize().height / (double) rows);
+            grid = new int[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    grid[i][j] = scan.nextInt();
+                }
+            }
+            this.paint = true;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
-    public File getFile() {
-        return this.file;
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(500, 400);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        if (!paint) {
+            return; // dont update
+        }
+        super.paintComponent(g);
+        for (int y = 0; y < cols; y++) {
+            for (int x = 0; x < rows; x++) {
+                TileType type = TileType.to(grid[x][y]);
+                switch (type) {
+                    case OPEN:
+                        g.setColor(Color.WHITE);
+                        break;
+                    case WALL:
+                        g.setColor(Color.BLUE);
+                        break;
+                    case TRIED:
+                        g.setColor(Color.PINK);
+                        break;
+                    case START:
+                        g.setColor(Color.BLACK);
+                        break;
+                    case END:
+                        g.setColor(Color.BLACK);
+                        break;
+                    case SOLVED:
+                        g.setColor(Color.GREEN);
+                        break;
+                }
+                g.fillRect(y * colSize, x * rowSize, colSize, rowSize);
+            }
+        }
+
+        g.setColor(Color.black);
+        for (int r = 0; r <= rows; r++) {
+            int size = r * rowSize;
+            g.drawLine(0, size, getPreferredSize().width + 3 - colSize, size);
+        }
+        for (int c = 0; c <= cols; c++) {
+            int size = c * colSize;
+            g.drawLine(size, 0, size, getPreferredSize().height);
+        }
+    }
+
+    public TileType getTooltip() {
+        return this.tooltip;
+    }
+
+    public void setTooltip(TileType tooltip) {
+        this.tooltip = tooltip;
+    }
+
+    public int getCols() {
+        return this.cols;
+    }
+
+    public int getColSize() {
+        return this.colSize;
+    }
+
+    public int getRows() {
+        return this.rows;
+    }
+
+    public int getRowSize() {
+        return this.rowSize;
     }
 }
