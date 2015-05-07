d796889f930dfc82b943d98438c647a609b9c531
Nick Porillo <nsporillo@gmail.com>
2015-05-07 12:51:25 -0400

    Switch to double precision
    This fixes the issue where a maze grid wouldn’t fit perfectly into it’s
    panel always. Due to the column or row sizing being limits to integers
    with a constant parent frame size..

--- src/Maze.java
+++ src/Maze.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.math.BigDecimal;
 
 /**
  * Author: Nick Porillo
@@ -13,7 +16,8 @@ public class Maze extends JPanel {
     private Point start, finish;
 
     private String name;
-    private int rows, cols, rowSize, colSize;
+    private int rows, cols;
+    private double rowSize, colSize;
     protected boolean solved = false, paint = false;
 
     protected int[][] grid;
@@ -49,8 +53,8 @@ public class Maze extends JPanel {
      * Handles various maze loading tasks
      */
     public void load() {
-        this.colSize = (int) ((double) getPreferredSize().width / (double) this.cols);
-        this.rowSize = (int) ((double) getPreferredSize().height / (double) this.rows);
+        this.colSize = getPreferredSize().getWidth() / (double) this.cols;
+        this.rowSize = getPreferredSize().getHeight() / (double) this.rows;
         this.tooltip = TileType.START;
         this.paint = true;
         this.solved = false;
@@ -72,6 +76,8 @@ public class Maze extends JPanel {
 
     /**
      * Handles rendering the maze
+     * Use double precision! Our row and column sizes aren't always even ints!
+     * Our app height and width is constant, so the sizes are often decimals
      *
      * @param g graphics
      */
@@ -80,26 +86,27 @@ public class Maze extends JPanel {
         if (!paint) {
             return; // dont update
         }
-        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         // render each tile in grid
         for (int y = 0; y < cols; y++) {
             for (int x = 0; x < rows; x++) {
                 TileType type = TileType.to(grid[x][y]);
-                g.setColor(type.color());
-                g.fillRect(y * colSize, x * rowSize, colSize, rowSize);
+                g2.setColor(type.color());
+                g2.fill(new Rectangle2D.Double(y * colSize, x * rowSize, colSize, rowSize));
             }
         }
 
         // render black grid lines
-        g.setColor(Color.black);
+        g2.setColor(Color.black);
         for (int r = 0; r <= rows; r++) {
-            int size = r * rowSize;
-            g.drawLine(0, size, getPreferredSize().width, size);
+            double size = r * rowSize;
+            g2.draw(new Line2D.Double(0, size, this.getWidth(), size));
         }
         for (int c = 0; c <= cols; c++) {
-            int size = c * colSize;
-            g.drawLine(size, 0, size, getPreferredSize().height);
+            double size = c * colSize;
+            g2.draw(new Line2D.Double(size, 0, size, this.getHeight()));
         }
     }
 
@@ -181,20 +188,33 @@ public class Maze extends JPanel {
     }
 
     /**
+     * Gets if a raw point is within the grid
+     *
+     * @param raw grid point
+     * @return if point is within grid
+     */
+    public boolean isTile(Point raw) {
+        if (raw.x < 0 || raw.x > getWidth() || raw.y < 0 || raw.y > getHeight()) {
+            // raw point is outside maze panel
+            return false;
+        }
+        Point tile = convert(raw);
+        // check if tile is inside the grid
+        return !(tile.x < 0 || tile.x >= cols || tile.y < 0 || tile.y >= rows);
+    }
+
+    /**
      * Gets if a point in the grid is OPEN
      *
      * @param tile Search tile
      * @return if tile is free
      */
     private boolean isOpenTile(Point tile) {
-        int x = tile.x;
-        int y = tile.y;
-        // check if tile is inside the grid
-        if (x < 0 || x >= rows || y < 0 || y >= cols) {
+        if (tile.x < 0 || tile.x >= rows || tile.y < 0 || tile.y >= cols) {
             return false;
         }
         // check if tile is open or the end tile(needs to be open so we can solve)
-        return grid[x][y] == TileType.OPEN.value() || grid[x][y] == TileType.END.value();
+        return grid[tile.x][tile.y] == TileType.OPEN.value() || grid[tile.x][tile.y] == TileType.END.value();
     }
 
     /**
@@ -235,30 +255,43 @@ public class Maze extends JPanel {
      * @return grid point value
      */
     public int getValueAt(int rawX, int rawY) {
-        int x = (rawX / colSize) % cols;
-        int y = (rawY / rowSize) % rows;
-        return grid[y][x];
+        Point tile = convert(new Point(rawX, rawY));
+        return grid[tile.y][tile.x];
     }
 
     /**
-     * Sets a grid point to a TileType
+     * Converts a raw mouse point to a grid point
+     * Uses BigDecimal for accurate modulus operations
      *
-     * @param tile grid point
+     * @param raw raw mouse point
+     * @return grid point
+     */
+    private Point convert(Point raw) {
+        BigDecimal bdX = new BigDecimal(raw.x / colSize);
+        BigDecimal bdCols = new BigDecimal(cols);
+        BigDecimal bdY = new BigDecimal(raw.y / rowSize);
+        BigDecimal bdRows = new BigDecimal(rows);
+        int x = bdX.remainder(bdCols).intValue();
+        int y = bdY.remainder(bdRows).intValue();
+        return new Point(x, y);
+    }
+
+    /**
+     * Sets a raw point to a TileType
+     *
+     * @param raw  grid point
      * @param type new type
      * @return if successful
      */
-    public boolean setValueAt(Point tile, TileType type) {
-        int rawX = (int) tile.getX();
-        int rawY = (int) tile.getY();
-        TileType old = TileType.to(getValueAt(rawX, rawY));
-
+    public boolean setValueAt(Point raw, TileType type) {
+        TileType old = TileType.to(getValueAt(raw.x, raw.y));
         if (type != old) {
             if (type.isExclusive() && this.findValue(type) != null) {
+                setTooltip(TileType.WALL); // auto set tooltip to wall :)
                 return false; // can't set more than one exclusive tile
             }
-            int x = (rawX / colSize) % cols;
-            int y = (rawY / rowSize) % rows;
-            grid[y][x] = type.value();
+            Point tile = convert(raw);
+            grid[tile.y][tile.x] = type.value();
             if (old.isExclusive() || type.isExclusive()) {
                 // we made a tile change involving exclusive types
                 // good idea to update refs to avoid null pointers
@@ -348,32 +381,5 @@ public class Maze extends JPanel {
     @Override
     public void setName(String name) {
         this.name = name;
-    }
-
-    /**
-     * Sets rows of maze
-     *
-     * @param rows new # of rows
-     */
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    /**
-     * Sets columns of maze
-     *
-     * @param cols new # of columns
-     */
-    public void setCols(int cols) {
-        this.cols = cols;
-    }
-
-    /**
-     * Sets new maze grid
-     *
-     * @param grid new maze grid
-     */
-    public void setGrid(int[][] grid) {
-        this.grid = grid;
     }
 }
