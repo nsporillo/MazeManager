1c48431c788e599ead318b0d06f580ccd4f21d81
Nick Porillo <nsporillo@gmail.com>
2015-05-07 04:54:50 -0400

    Add comments
    Also remove creating maze directory if it doesn’t exist on load, it is
    not needed.
    
    Also explicitly flush print writer buffer, even though closing will
    flush before it closes too.

--- src/MazeFactory.java
+++ src/MazeFactory.java
@@ -5,19 +5,29 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class MazeFactory {
-    private static String root = "mazes";
+    private static String root = "mazes"; // mazes directory
 
-    static {
-        File dir = new File(root);
-        if (!dir.exists()) {
-            dir.mkdir();
-        }
-    }
 
+
+    /**
+     * Generates an initial, empty maze
+     *
+     * @param name maze name
+     * @param rows number of rows
+     * @param cols number of columns
+     * @return {@link Maze} object
+     */
     public static Maze initialMaze(String name, int rows, int cols) {
         return new Maze(name, getEmptyGrid(rows, cols));
     }
 
+    /**
+     * Loads maze from file
+     *
+     * @param name name of file to load
+     * @return {@link Maze} object
+     * @throws IOException
+     */
     public static Maze load(String name) throws IOException {
         File file = new File(root, name + ".maze");
         Scanner scan = new Scanner(file);
@@ -32,24 +42,38 @@ public class MazeFactory {
         return new Maze(name, grid);
     }
 
+    /**
+     * Saves maze to file
+     *
+     * @param maze Maze object to save
+     * @throws IOException
+     */
     public static void save(Maze maze) throws IOException {
         maze.reset(); // clear out tried/solved tiles
         PrintWriter pw = new PrintWriter(new File(root, maze.getName() + ".maze"));
         String one = maze.getRows() + " " + maze.getCols();
-        pw.println(one);
+        pw.println(one); // print row and column to file
         for (int i = 0; i < maze.getRows(); i++) {
             for (int j = 0; j < maze.getCols(); j++) {
                 pw.print(maze.grid[i][j] + " ");
             }
-            pw.println();
+            pw.println(); // new line for next row
         }
-        pw.close();
-
+        pw.flush(); // flush buffer
+        pw.close(); // close resource
     }
 
+    /**
+     * Generates a maze grid with all open tiles
+     *
+     * @param rows number of rows
+     * @param cols number of columns
+     * @return 3d int array filled with open tiles
+     */
     public static int[][] getEmptyGrid(int rows, int cols) {
         int[][] grid = new int[rows][cols];
-        for(int[] row: grid) {
+        for (int[] row : grid) {
+            // fill each row with open tiles
             Arrays.fill(row, TileType.OPEN.value());
         }
         return grid;
